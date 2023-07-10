bc3972c0182d37165eb71d07ff65013e17c04842
Manucher <manucher20056@gmail.com>
2023-07-10 13:43:31 +0500

    '.'

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
+// Components
 import Title from './ui/Title/Title'
 import Subtitile from './ui/Subtitle/Subtitle'
+import Paragraph from './ui/Paragraph/Paragraph'
+import Link from './ui/Link/Link'
+import CheckBox from './ui/CheckBox/CheckBox'
+import Button from './ui/Button/Button'
+import SVGButton from './ui/SVGButton/SVGButton'
 
-export { Title, Subtitile }
+// Styles
+import './assets/styles/index.scss'
+
+export { Title, Subtitile, Paragraph, Link, CheckBox, Button, SVGButton }
