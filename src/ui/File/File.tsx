c315db3519c21e7c2cb2688406196bbfb931fac2
Manucher <manucher20056@gmail.com>
2023-08-11 23:54:09 +0500

    '.'

---
+++ src/ui/File/File.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+import classes from './File.moduel.scss'
+
+export interface IFile {}
+
+export const File: FC<IFile> = () => {
+	return <div>File</div>
+}
---
+++ src/ui/File/Variant/DragAndDrop/DragAndDrop.tsx
@@ -0,0 +1,8 @@
+import type { FC } from 'react'
+import classes from './DragAndDrop.module.scss'
+
+export interface IDragAndDrop {}
+
+export const DragAndDrop: FC<IDragAndDrop> = () => {
+	return <div>DragAndDrop</div>
+}
