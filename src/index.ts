3cbf2ddd6a8412f0808fb1c5bffca4966ca69563
015 <015@first.local>
2023-09-18 12:40:16 +0500

    '.'

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,26 @@
 // UI
-import { Title } from './ui/Title/Title'
-import { Subtitle } from './ui/Subtitle/Subtitle'
-import { Paragraph } from './ui/Paragraph/Paragraph'
-import { Link } from './ui/Link/Link'
-import { CheckBox } from './ui/CheckBox/CheckBox'
-import { Button } from './ui/Button/Button'
-import { SVGButton } from './ui/SVGButton/SVGButton'
-import { Radio } from './ui/Radio/Radio'
-import { Input } from './ui/Input/Input'
-import { TextError } from './ui/TextError/TextError'
-import { Form } from './ui/Form/Form'
+export { Title } from './ui/Title/Title'
+export { Subtitle } from './ui/Subtitle/Subtitle'
+export { Paragraph } from './ui/Paragraph/Paragraph'
+export { Link } from './ui/Link/Link'
+export { CheckBox } from './ui/CheckBox/CheckBox'
+export { Button } from './ui/Button/Button'
+export { SVGButton } from './ui/SVGButton/SVGButton'
+export { Radio } from './ui/Radio/Radio'
+export { Input } from './ui/Input/Input'
+export { TextError } from './ui/TextError/TextError'
+export { Form } from './ui/Form/Form'
 
 // Components
-import { SettingsPanel } from './components/SettingsPanel/SettingsPanel'
-import { Loader } from './components/Loader/Loader'
-import { Popup } from './components/Popup/Popup'
-import { Spinner } from './components/Spinner/Spinner'
-import { Accordion } from './components/Accordion/Accordion'
-import { NotFound } from './components/NotFound/NotFound'
-import { GoogleAuth } from './components/GoogleAuth/GoogleAuth'
-import { VKAuth } from './components/VKAuth/VKAuth'
-import { ConfirmEmail } from './components/ConfirmEmail/ConfirmEmail'
+export { SettingsPanel } from './components/SettingsPanel/SettingsPanel'
+export { Loader } from './components/Loader/Loader'
+export { Popup } from './components/Popup/Popup'
+export { Spinner } from './components/Spinner/Spinner'
+export { Accordion } from './components/Accordion/Accordion'
+export { NotFound } from './components/NotFound/NotFound'
+export { GoogleAuth } from './components/GoogleAuth/GoogleAuth'
+export { VKAuth } from './components/VKAuth/VKAuth'
+export { ConfirmEmail } from './components/ConfirmEmail/ConfirmEmail'
 
 // Styles
 import '@/assets/styles/index.scss'
-
-export {
-	Title,
-	Subtitle,
-	Paragraph,
-	ConfirmEmail,
-	Link,
-	CheckBox,
-	Button,
-	SVGButton,
-	GoogleAuth,
-	Radio,
-	SettingsPanel,
-	Loader,
-	Popup,
-	Spinner,
-	Accordion,
-	Input,
-	NotFound,
-	Form,
-	VKAuth,
-	TextError,
-}
