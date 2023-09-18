09aa616b00ee639d87d5d8a5bd5c251e82e50a1b
015 <015@first.local>
2023-09-18 14:56:04 +0500

    '.'

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,46 @@
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import path from 'path'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import { extname, relative, resolve } from 'path'
+import { fileURLToPath } from 'node:url'
+import { glob } from 'glob'
+
+const input = Object.fromEntries(
+	glob.sync('src/**/*.{ts,tsx}', {
+		ignore: "src/**/*.stories.tsx"
+	}).map(file => [
+		relative(
+			'src',
+			file.slice(0, file.length - extname(file).length)
+		),
+		fileURLToPath(new URL(file, import.meta.url))
+	])
+)
 
 export default defineConfig({
-	plugins: [dts()],
+	plugins: [dts(), libInjectCss(),],
 	build: {
 		lib: {
 			entry: 'src/index.ts',
-			name: 'kuui',
 			fileName: 'index',
-			formats: ['es', "umd"]
+			formats: ['es']
 		},
 		copyPublicDir: true,
 		rollupOptions: {
+			input,
 			external: ['react'],
 			output: {
 				globals: {
 					react: 'react',
-				}
+				},
+				assetFileNames: 'assets/[name][extname]',
+				entryFileNames: '[name].js',
 			},
 		},
 	},
 	resolve: {
 		alias: {
-			'@': path.resolve('./src'),
+			'@': resolve('./src'),
 		},
 	},
 })
