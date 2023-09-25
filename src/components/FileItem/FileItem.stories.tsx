22f230972437c7a46de88cba6bc11f4d76b1f951
Manucher <manucher20056@gmail.com>
2023-09-25 11:13:09 +0500

    '.'

---
+++ src/components/FileItem/FileItem.stories.tsx
@@ -0,0 +1,26 @@
+// Types
+import type { Meta, StoryObj } from '@storybook/react'
+
+// Components
+import { FileItem } from './FileItem'
+
+// Styles
+import '@/assets/styles/index.scss'
+
+// Setting FileItem
+const meta: Meta<typeof FileItem> = {
+	component: FileItem,
+}
+
+export default meta
+
+// Making Story
+type Story = StoryObj<typeof FileItem>
+
+// Setting Story
+export const Playground: Story = {
+	args: {
+		extension: 'word',
+		name: 'Mike smith',
+	},
+}
