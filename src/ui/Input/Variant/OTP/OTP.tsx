66dabcffdf8dcfa955649bb19a23062551bb9600
Manucher <manucher20056@gmail.com>
2023-10-08 22:20:22 +0500

    '.'

--- src/ui/Input/Variant/OTP/OTP.tsx
+++ src/ui/Input/Variant/OTP/OTP.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import { useEffect, useRef, type FC } from 'react'
+import { KeyboardEvent, useEffect, useRef, useState, type FC } from 'react'
 import { Input } from './Input/Input'
 import classes from './OTP.module.scss'
 
@@ -10,14 +10,30 @@ export interface IOTP {
 }
 
 export const OTP: FC<IOTP> = ({ value, length, onChange }) => {
+	const [focusIdx, setFocusIdx] = useState<number>(0)
+	const [prevValue, setPrevValue] = useState<string>('')
+
 	const arr = new Array(length).fill(null)
 
 	const inputRefs = useRef<(HTMLInputElement | null)[]>([])
 
+	function keyDownHandler(e: KeyboardEvent<HTMLInputElement>) {
+		if (e.code === 'Backspace') {
+			setFocusIdx(prev => prev - 1)
+		}
+	}
+
 	useEffect(() => {
-		const idx = value.length
-		inputRefs.current[idx]?.focus()
+		if (value.length > prevValue.length) {
+			setFocusIdx(prev => prev + 1)
+		}
+
+		setPrevValue(value)
 	}, [value])
+
+	useEffect(() => {
+		inputRefs.current[focusIdx]?.focus()
+	}, [focusIdx])
 
 	const styles = clsx([classes.root])
 
@@ -25,6 +41,7 @@ export const OTP: FC<IOTP> = ({ value, length, onChange }) => {
 		<div className={styles}>
 			{arr.map((_, idx) => (
 				<Input
+					onKeyDown={keyDownHandler}
 					ref={el => (inputRefs.current[idx] = el)}
 					key={idx}
 					onChange={e => onChange(value.replaceAt(idx, e.target.value))}
