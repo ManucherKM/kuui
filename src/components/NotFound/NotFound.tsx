923ae01148da5a7789cd0a94bcb3657ebf1c5226
ManucherKM <manucher20056@gmail.com>
2023-12-29 15:20:53 +0500

    fix bug

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -2,7 +2,17 @@
 import type { FC } from 'react'
 
 // Styles
+import clsx from 'clsx'
 import classes from './NotFound.module.scss'
+
+export enum ENotFoundFill {
+	screen = 'screen',
+	all = 'all',
+}
+
+export interface INotFound {
+	fill?: `${ENotFoundFill}`
+}
 
 /**
  * The NotFound component is a page that will appear if the user has taken a
@@ -11,9 +21,10 @@ import classes from './NotFound.module.scss'
  * @example
  * 	;<NotFound />
  */
-export const NotFound: FC = () => {
+export const NotFound: FC<INotFound> = ({ fill = ENotFoundFill.screen }) => {
+	const rootStyles = clsx([classes.root, classes[fill]])
 	return (
-		<div className={classes.root}>
+		<div className={rootStyles}>
 			<span className={classes.title}>Error 404</span>
 			<span className={classes.subtitle}>Page not found</span>
 		</div>
